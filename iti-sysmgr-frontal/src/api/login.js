f96aab631dd1568f515e9ba9c29b6e29d9db7131
weistar <weistar103@163.com>
2019-01-01 22:02:32 +0800

    add iti-sysmgr-frontal

---
+++ iti-sysmgr-frontal/src/api/login.js
@@ -0,0 +1,30 @@
+import qs from 'qs'
+import request from '@/utils/request'
+
+export function loginByUsername(username, password) {
+  const data = {
+    account: username,
+    password
+  }
+  return request({
+    url: '/login/simple',
+    method: 'post',
+    data: qs.stringify(data)
+  })
+}
+
+export function logout() {
+  return request({
+    url: '/login/logout',
+    method: 'post'
+  })
+}
+
+export function getUserInfo(token) {
+  return request({
+    url: '/user/info',
+    method: 'get',
+    params: { token }
+  })
+}
+
